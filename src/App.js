779ec01605308e240a6ee3138abd810041e1a48a
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2022-10-05 20:04:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import data from 'data.js'
+
+function App() {
+  return (
+    <div>
+      Travel Journal
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    title: "",
+    location: "",
+    googleMapsUrl: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    imageUrl: "",
+  },
+  {
+    title: "",
+    location: "",
+    googleMapsUrl: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    imageUrl: "",
+  },
+  {
+    title: "",
+    location: "",
+    googleMapsUrl: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    imageUrl: ""
+  }
+]
